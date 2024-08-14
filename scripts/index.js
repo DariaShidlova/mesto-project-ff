0d965e2e028acdf695415a68b6929f7ae989e3fd
Daria Shidlova <daryashidlova@yandex.ru>
2024-08-14 19:48:49 +0300

    написан код js

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+// добавление карточки
+function createCard(cardData) {
+    const template = document.querySelector('#card-template').content;
+    const cardElement = template.cloneNode(true); //копирование контента
+
+    // установка значений в карточке
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__image').alt = cardData.name;
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+    return cardElement;
+}
+
+const cardList = document.querySelector('.places__list'); //поиск элемента для отображения карточек
+
+//добавление карточек на страницу
+initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData);
+    cardList.append(cardElement);
+});
+
+// удаление карточки
+function deleteCard(event) {
+    event.target.closest('.card').remove();
+}
