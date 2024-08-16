7eb2a938dc522d150464f4f463846dcb199dfaca
Daria Shidlova <daryashidlova@yandex.ru>
2024-08-16 17:00:55 +0300

    исправлены замечания после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -9,30 +9,31 @@
 // @todo: Вывести карточки на страницу
 
 
-// добавление карточки
-function createCard(cardData) {
-    const template = document.querySelector('#card-template').content;
-    const cardElement = template.cloneNode(true); //копирование контента
 
-    // установка значений в карточке
+// добавление карточки
+function createCard(cardData, deleteCallback) {
+    const template = document.querySelector('#card-template').content.querySelector('.places__item');
+    const cardElement = template.cloneNode(true);
+
+// установка значений в карточке
     cardElement.querySelector('.card__title').textContent = cardData.name;
     cardElement.querySelector('.card__image').src = cardData.link;
     cardElement.querySelector('.card__image').alt = cardData.name;
 
-    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCallback); 
 
     return cardElement;
 }
-
-const cardList = document.querySelector('.places__list'); //поиск элемента для отображения карточек
+//поиск элемента для отображения карточек
+const cardList = document.querySelector('.places__list');
 
 //добавление карточек на страницу
 initialCards.forEach(cardData => {
-    const cardElement = createCard(cardData);
+    const cardElement = createCard(cardData, deleteCard);
     cardList.append(cardElement);
 });
 
-// удаление карточки
+//удаление карточки
 function deleteCard(event) {
     event.target.closest('.card').remove();
 }
